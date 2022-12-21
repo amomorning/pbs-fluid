35f51c508463de32728e190e6e67683f470a934a
Yichen Mo <amomorning@gmail.com>
2022-12-21 17:59:27 +0100

    Add command line helper

--- plume_sim.py
+++ plume_sim.py
@@ -1,82 +1,94 @@
 import taichi as ti
+import click
 from Plume2d import Plume2d
 from Renderer import Renderer
 from Solid import SolidBox, SolidSphere
 
-ti.init(arch=ti.cuda)
 
-res_x = 256
-res_y = int(res_x * 1)
-dx = 1
-dt = 0.005 * ti.sqrt((res_x + res_y) * 0.5) *.5
-accuracy = 1e-4
-n_iters = 100
-advection = "MAC"       # SL, MAC, FLIP
-interpolation = "cerp"  # bilerp, cerp
-integration = "rk3"     # euler, rk3
-solver = "CG"           # MIC, CG, GS 
-reflection = True        # True, False
-wind = False
-bodies = [
-    # SolidBox(.55, .45, .58, .07, -.7),
-    # SolidSphere(.7, .3, .15),
-    ]
+@click.command()
+@click.option('--gpu/--no-gpu', default=True, help='Use cuda if gpu is available', show_default=True)
+@click.option('-x', '--res_x', default=256, help='Resolution x', show_default=True)
+@click.option('-y', '--res_y', default=256, help='Resolution y', show_default=True)
+@click.option('--dx', default=1, help='dx', show_default=True)
+@click.option('--dt', default=0.04, help='dt', show_default=True)
+@click.option('--accuracy', default=1e-4, help='accuracy', show_default=True)
+@click.option('--n_iters', default=100, help='number of iterations', show_default=True) #TODO
+@click.option('--substeps', default=20, help='number of substeps', show_default=True)
+@click.option('-a', '--advection', default='MAC', type=click.Choice(['MAC', 'SL', 'FLIP'], case_sensitive=False), help='advection method', show_default=True)
+@click.option('-e', '--interpolation', default='cerp', type=click.Choice(['cerp', 'bilerp'], case_sensitive=False), help='interpolation method', show_default=True)
+@click.option('-i', '--integration', default='rk3', type=click.Choice(['rk3', 'euler'], case_sensitive=False), help='integration method', show_default=True)
+@click.option('-s', '--solver', default='GS', type=click.Choice(['GS', 'CG', 'MIC'], case_sensitive=False), help='solver method', show_default=True)
+@click.option('-r', '--reflection', default=False, is_flag=True, help='boolean flag to use reflection')
+@click.option('-w', '--wind', default=False, is_flag=True, help='boolean flag to add wind')
+@click.option('-b', '--body', default=False, is_flag=True, help='boolean flag to add default solid bodies')
+@click.option('-o', '--output', default=False, is_flag=True, help='boolean flag for output step png to output folder')
+def main(gpu, res_x, res_y, dx, dt, accuracy, n_iters, 
+        advection, interpolation, integration, solver, 
+        reflection, wind, body, output, substeps):
+    if gpu: ti.init(arch=ti.cuda)
+    else: ti.init(arch=ti.cpu)
 
-args = {
-    'res_x': res_x,
-    'res_y': res_y,
-    'dx': dx,
-    'dt': dt,
-    'accuracy': accuracy,
-    'poisson_iters': n_iters,
-    'wind': wind,
-    'advection': advection,             
-    'interpolation': interpolation,    
-    'integration': integration,        
-    'solver': solver,
-    'reflection': reflection,           
-    'bodies': bodies,
-}
+    bodies = [
+        SolidBox(.5, .6, .7, .1, -.7),
+        SolidSphere(.7, .3, .15),
+        ] if body else []
 
-plume = Plume2d(args)
+    args = {
+        'res_x': res_x,
+        'res_y': res_y,
+        'dx': dx,
+        'dt': dt,
+        'accuracy': accuracy,
+        'poisson_iters': n_iters,
+        'wind': wind,
+        'advection': advection,             
+        'interpolation': interpolation,    
+        'integration': integration,        
+        'solver': solver,
+        'reflection': reflection,           
+        'bodies': bodies,
+    }
 
-# For rendering
-renderer = Renderer(res_x, res_y, dx, plume._cell)
+    plume = Plume2d(args)
 
-
-window = ti.ui.Window("Plume 2d", (res_x*2, res_y*2),
-                      vsync=True)
-canvas = window.get_canvas()
-canvas.set_background_color((0, 0, 0))
-scene = ti.ui.Scene()
-camera = ti.ui.Camera()
-
-substeps = 20
-frame = 0
-
-while window.running:
-    if frame > 1000:
-        break
-
-    for _ in range(substeps):
-        plume.substep()
-    renderer.render(plume.density, ti.Vector([0,0,0]), ti.Vector([1,1,1]))
+    # For rendering
+    renderer = Renderer(res_x, res_y, dx, plume._cell)
 
 
-    camera.position(plume.res_x / 2, plume.res_y / 2, 300)
-    camera.lookat(plume.res_x / 2, plume.res_y / 2, 0)
-    scene.set_camera(camera)
+    window = ti.ui.Window("Plume 2d", (res_x*2, res_y*2),
+                        vsync=True)
+    canvas = window.get_canvas()
+    canvas.set_background_color((0, 0, 0))
+    scene = ti.ui.Scene()
+    camera = ti.ui.Camera()
 
-    scene.ambient_light((1, 1, 1))
-    scene.mesh(vertices=renderer.V,
-               indices=renderer.F,
-               per_vertex_color=renderer.C,
-            #    show_wireframe=True
-               )
+    frame = 0
 
-    canvas.scene(scene)
-    
-    window.save_image(f"./output/{advection}_{interpolation}_{integration}_{solver}_{reflection}_{frame:05d}.png")
-    window.show()
+    while window.running:
 
-    frame += 1
+        for _ in range(substeps):
+            plume.substep()
+        renderer.render(plume.density, ti.Vector([0,0,0]), ti.Vector([1,1,1]))
+
+
+        camera.position(plume.res_x / 2, plume.res_y / 2, 300)
+        camera.lookat(plume.res_x / 2, plume.res_y / 2, 0)
+        scene.set_camera(camera)
+
+        scene.ambient_light((1, 1, 1))
+        scene.mesh(vertices=renderer.V,
+                indices=renderer.F,
+                per_vertex_color=renderer.C,
+                #    show_wireframe=True
+                )
+
+        canvas.scene(scene)
+        
+        if output:
+            window.save_image(f"./output/{advection}_{interpolation}_{integration}_{solver}_{reflection}_{frame:05d}.png")
+        window.show()
+
+        frame += 1
+
+if __name__ == "__main__":
+    main()
